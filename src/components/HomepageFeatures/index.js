52ee99b320c4b12d6581cc8d5a42475dad31d00d
xpecnh2n@gmail.com <xpeggs@163.com>
2025-02-06 21:36:31 +0800

    Revert "Customed HomepageFeatures."
    This reverts commit bc8cb7ad3d03d6f2f74e582ac9c76721ba5e3028.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,11 +4,32 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: '2025 新年快乐!',
-    Png: require('@site/static/img/2025sfGroupPhoto.png').default,
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        2025.01.29
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by React',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
       </>
     ),
   },
