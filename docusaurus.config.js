3b725251d25e706ed8e4973902eaf3ad5e6daad5
3x-Ice-Cream <xpecnh2n@gmail.com>
2025-02-23 12:41:54 +0800

    Apply new logo.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
         title: 'Stardrop Docs',
         logo: {
           alt: 'Stardrop Pavilion Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
